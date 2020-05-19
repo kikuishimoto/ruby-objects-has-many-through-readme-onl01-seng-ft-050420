321189e4ccfbb3b5492a61b19b8e2f433b9b8f0c
kiku ishimoto <kikuish@gmail.com>
2020-05-19 22:12:33 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,9 +1,10 @@
 class Meal
+
   attr_accessor :waiter, :customer, :total, :tip
 
   @@all = []
 
-  def initialize(waiter, customer, total, top = 0)
+  def initialize(waiter, customer, total, tip=0)
     @waiter = waiter
     @customer = customer
     @total = total
