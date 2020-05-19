49ef55f9b59cba5329576d19085d2a16568ea3e1
kiku ishimoto <kikuish@gmail.com>
2020-05-19 21:37:18 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -2,10 +2,10 @@ class Waiter
   attr_accessor :name, :yrs_experience
   @@all = []
   def initialize(name, yrs_experience)
-    @name = name 
+    @name = name
     @yrs_experience = yrs_experience
-    @@all << self 
-  end 
+    @@all << self
+  end
   def self.all
     @@all
   end
